1c059f858521ec19a556f7b5d7927e7265ca9224
Massimo Zerbini <massimo.zerbini@gmail.com>
2021-10-16 18:32:48 +0200

    chore fromenv library added for configuration initialization #2

--- cmd/applico-api/main.go
+++ cmd/applico-api/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"log"
+
 	_ "github.com/lib/pq"
 	"github.com/maxzerbini/applico/config"
 	"github.com/maxzerbini/applico/store"
 )
 
 func main() {
-	config.InitConfig()
-	store.InitDatabaseConn()
+	conf, err := config.ConfigurationFromEnv()
+	if err != nil {
+		log.Printf("Configuration initialization error: %v", err)
+		return
+	}
+	store.InitDatabaseConn(conf.DatabaseConfiguration())
 	defer store.DbManager.Close()
 
 }
